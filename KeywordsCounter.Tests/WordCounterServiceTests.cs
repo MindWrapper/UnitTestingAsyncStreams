6ebeb30fa641b62635b02b707fb0214ee6e447f7
Yan Drugalya <yan@unity3d.com>
2020-04-26 17:38:34 +0200

    fail: GetWordCountUpdates_NoWordsDataInStream_ReturnsZeroForEachWordToCount

--- KeywordsCounter.Tests/WordCounterServiceTests.cs
+++ KeywordsCounter.Tests/WordCounterServiceTests.cs
@@ -28,5 +28,25 @@ namespace KeywordsCounter.Tests
            
             Assert.That(result.IsEmpty);
         }
+
+        [Test]
+        public async Task GetWordCountUpdates_NoWordsDataInStream_ReturnsZeroForEachWordToCount()
+        {
+            var dataSource = new Mock<IDataSource>();
+            var wordsToCount = new[] { "foo", "bar" };
+            var streamData = new string[] { };
+            dataSource.Setup(x => x.GetData()).Returns(streamData.ToAsyncEnumerable());
+            var service = new WordCounterService(dataSource.Object);
+            var result = new ConcurrentBag<KeyValuePair<string, int>>();
+
+            await foreach (var update in service.GetWordCountUpdates(wordsToCount))
+            {
+                result.Add(update);
+            }
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result.First().Value, Is.EqualTo(0));
+            Assert.That(result.Last().Value, Is.EqualTo(0));
+        }
     }
 }
