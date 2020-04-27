36cb4e227cf9d7b06a679fa87af9f9c2c487ca86
Yan Drugalya <yan@unity3d.com>
2020-04-27 18:54:50 +0200

    Added tests to demonstrate how to use exceptions

---
+++ KeywordsCounter.Tests/ExceptionTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using WordCounter;
+
+namespace KeywordsCounter.Tests
+{
+    [TestFixture]
+    public class ExceptionTests
+    {
+        [Test]
+        public void GetWordCountUpdates_DataSourceThroesAndException_RethrowsTheException()
+        {
+            var dataSource = new Mock<IDataSource>();
+            async IAsyncEnumerable<string> StreamData()
+            {
+                await Task.FromException(new IOException("hard disk is corrupted"));
+                yield break; // otherwise compiler will complain "not all path return a value"
+            }
+            dataSource.Setup(x => x.GetData()).Returns(StreamData);
+            var service = new WordCounterService(dataSource.Object);
+
+            var firstUpdate = service.GetWordCountUpdates(new[] { "foo" }).FirstAsync();
+
+            Assert.That(async () => await firstUpdate, Throws.InstanceOf<IOException>());
+        }
+    }
+}
