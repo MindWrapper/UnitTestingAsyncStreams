8e136d0e6dccd1fe143958a931e1a7a188a1c3d7
Yan Drugalya <yan@unity3d.com>
2020-04-29 19:42:07 +0200

    Putting it all together

---
+++ KeywordsMonitor/FileDataSource.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using WordCounter;
+
+namespace KeywordsMonitor
+{
+    class FileDataSource : IDataSource
+    {
+        readonly string m_FileName;
+
+        public FileDataSource(string fileName)
+        {
+            m_FileName = fileName;
+        }
+
+        public async IAsyncEnumerable<string> GetData()
+        {
+            using var reader = File.OpenText(m_FileName);
+            string line = null;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                yield return line;
+            }
+        }
+    }
+}
---
+++ KeywordsMonitor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WordCounter;
+
+namespace KeywordsMonitor
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var wordsToCount = new[] { "war", "peace" };
+            var dataSource = new FileDataSource("WarAndPeace.txt");
+            var wordCountService = new WordCounterService(dataSource);
+            var warCount = 0;
+            var peaceCount = 0;
+            var linesProcessed = 0;
+
+            var progress = new Progress<int>(n => linesProcessed += n);
+            await foreach (var update in wordCountService.GetWordCountUpdates(wordsToCount, CancellationToken.None, progress))
+            {
+                if (update.Word == "war")
+                {
+                    warCount += update.OccurrencesCount;
+                }
+                else
+                {
+                    peaceCount += update.OccurrencesCount;
+                }
+               
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"Lines processed: {linesProcessed} war: {warCount} peace {peaceCount}");
+            }
+        }
+    }
+}
